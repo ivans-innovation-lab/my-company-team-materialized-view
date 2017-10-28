f7e3db4357d2b859b261ffd66208e26d9fa0ff81
Ivan Dugalic <ivan.dugalic@namics.com>
2017-10-28 18:38:06 +0200

    Implementing the event handler

--- src/main/java/com/idugalic/queryside/team/domain/Team.java
+++ src/main/java/com/idugalic/queryside/team/domain/Team.java
@@ -1,14 +1,17 @@
 package com.idugalic.queryside.team.domain;
 
-import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Version;
-import com.idugalic.common.blog.event.BlogPostCreatedEvent;
-import com.idugalic.common.blog.model.BlogPostCategory;
+
+import com.idugalic.common.team.event.TeamCreatedEvent;
+import com.idugalic.common.team.model.TeamStatus;
+import com.idugalic.queryside.project.domain.Project;
 
 /**
- * A JPA entity. It represents materialized view
+ * A JPA entity. It represents materialized view of TeamAggregate
  *
  */
 @Entity
@@ -16,28 +19,36 @@ public class Team {
 
     @Id
     private String id;
-
     @Version
     private Long version;
-
     private Long aggregateVersion;
+    private String name;
+    private String description;
+    private TeamStatus status;
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
 
-    private String authorId;
 
     public Team() {
     }
 
-    public Team(String id, Long aggregateVersion, String title, String rawContent, String renderContent, String publicSlug, Boolean draft, Boolean broadcast, Date publishAt, BlogPostCategory category, String authorId) {
+    public Team(String id, Long aggregateVersion, String name, String description, TeamStatus status) {
         super();
         this.id = id;
         this.aggregateVersion = aggregateVersion;
-        this.authorId = authorId;
+        this.name = name;
+        this.description = description;
+        this.status = status;
     }
 
-    public Team(BlogPostCreatedEvent event, Long aggregateVersion) {
+    public Team(TeamCreatedEvent event, Long aggregateVersion) {
         super();
         this.id = event.getId();
-        this.authorId = event.getAuthorId();
+        this.aggregateVersion = aggregateVersion;
+        this.name = event.getName();
+        this.description = event.getDescription();
+        this.status = event.getStatus();
     }
 
     public String getId() {
@@ -46,14 +57,6 @@ public class Team {
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getAuthorId() {
-        return authorId;
-    }
-
-    public void setAuthorId(String authorId) {
-        this.authorId = authorId;
     }
 
     public Long getVersion() {
@@ -71,4 +74,37 @@ public class Team {
     public void setAggregateVersion(Long aggregateVersion) {
         this.aggregateVersion = aggregateVersion;
     }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public TeamStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(TeamStatus status) {
+		this.status = status;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+    
 }
--- src/main/java/com/idugalic/queryside/team/handler/TeamViewEventHandler.java
+++ src/main/java/com/idugalic/queryside/team/handler/TeamViewEventHandler.java
@@ -7,7 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import com.idugalic.common.blog.event.BlogPostCreatedEvent;
+
+import com.idugalic.common.team.event.AssignProjectToTeamSucceededEvent;
+import com.idugalic.common.team.event.TeamCreatedEvent;
+import com.idugalic.queryside.project.repository.ProjectRepository;
 import com.idugalic.queryside.team.domain.Team;
 import com.idugalic.queryside.team.repository.TeamRepository;
 
@@ -23,10 +26,20 @@ public class TeamViewEventHandler {
 
     @Autowired
     private TeamRepository myAggregateRepository;
+    
+    @Autowired
+    private ProjectRepository projectRepository;
 
     @EventHandler
-    public void handle(BlogPostCreatedEvent event, @SequenceNumber Long version) {
+    public void handle(TeamCreatedEvent event, @SequenceNumber Long version) {
         LOG.info("TeamCreatedEvent: [{}] ", event.getId());
         myAggregateRepository.save(new Team(event, version));
+    }
+    
+    @EventHandler
+    public void handle(AssignProjectToTeamSucceededEvent event, @SequenceNumber Long version){
+    	 LOG.info("AssignProjectToTeamSucceededEvent: [{}] ", event.getId());
+    	 Team team = myAggregateRepository.findOne(event.getId());
+    	 team.setProject(projectRepository.findOne(event.getProjectId()));
     }
 }
